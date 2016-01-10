297839a94bfe3d688678c7e363525f7caa097010
knowlesc <knowlesc8@gmail.com>
2016-01-09 20:49:04 -0800

    Fix typo (did i mention testing = good)

--- routes/api.js
+++ routes/api.js
@@ -3,10 +3,13 @@ var express = require('express');
 var router = express.Router();
 
 router.post('/test', function(req, res, next) {
+  console.log(process.env.slackTeamId)
+  console.log(process.env.slackToken)
+
   if (req._body) {
     var body = req.body;
     if (body.team_id === process.env.slackTeamId &&
-        body.token === process.env.slackTolken) {
+        body.token === process.env.slackToken) {
       console.log(req.body)
     }
   }
