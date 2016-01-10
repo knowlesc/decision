6987f68b1553263b933af7fdca67193770ab39d5
knowlesc <knowlesc8@gmail.com>
2016-01-09 21:04:46 -0800

    Improve response and error handling

--- routes/api.js
+++ routes/api.js
@@ -4,31 +4,25 @@ var router = express.Router();
 
 router.post('/test', function(req, res, next) {
 
+  var response = {
+    "status": 400
+    "message": "Bad request"
+  };
+
   if (req._body) {
     var body = req.body;
-    if (body.team_id === process.env.slackTeamId) {
-      console.log("token good")
-    } else { console.log("token bad") }
-
-    if (body.token === process.env.slackToken) {
-      console.log("team id good")
-    } else { console.log("team id bad") }
-
     if (body.team_id === process.env.slackTeamId &&
         body.token === process.env.slackToken) {
-      console.log(req.body)
+      response = {
+        "response_type": "in_channel",
+        "text": "Custom Slack Command Test",
+      }
+      if (body.text) {
+        response.attachments = [{ "text": "You said " + body.text }];
+      }
     }
   }
 
-  var response = {
-    "response_type": "in_channel",
-    "text": "Text",
-    "attachments": [
-      {
-        "text":"Attachment"
-      }
-    ]
-  }
   res.json(response);
 });
 
